2ea0c6ad525ef40c31932434d8d721e2e93661f9
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-09-08 14:45:17 -0300

    exercicios 1 2 e 3 feitos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,18 @@
-const num = //seu numero aqui
+const num = Number(prompt('Insira um numero'))
+if(num%2===0){
+   console.log('divisivel por 2')
+   if(num%3===0){
+    console.log('divisivel por 3')
+   }else{
+      console.log('nao e divisivel por 3')
+   }
+} else{
+   console.log('nao e divisiel por 2')
+}
 
+
+
+
+// if(num%2===0 || num%3===0){
+//     console.log('ele e divisivel por 2 ou 3')
+// }
